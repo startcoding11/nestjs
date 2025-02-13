6267a0257c36457512c24ca1e7db0c89ea348e6c
githubdmn <dmninbox@gmail.com>
2025-02-13 19:58:57 +0100

    initial setup

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap()
+  .then(() => {
+    console.log(`Listening on port 3000...`);
+  })
+  .catch(() => {
+    console.error('Failed to start');
+  });
