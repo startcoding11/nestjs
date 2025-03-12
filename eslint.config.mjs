72093dfc8575e7883f10e5244b6eef6b7844ebcb
githubdmn <dmninbox@gmail.com>
2025-03-12 16:46:58 +0100

    linter LF

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default tseslint.config(
   },
   {
     rules: {
-      'prettier/prettier': ['error', { endOfLine: 'crlf' }],
+      'prettier/prettier': ['error', { endOfLine: 'lf' }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
