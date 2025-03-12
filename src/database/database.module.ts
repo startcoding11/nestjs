c33149bf9f0d4f7c45b6a02fe3c9766086353cca
githubdmn <dmninbox@gmail.com>
2025-03-12 07:48:04 +0100

    TODO: finish factory for sqlite

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfService } from '../conf/conf.service';
 import { Admin } from '@/api/core/admin/entities/admin.entity';
+import { ConfModule } from '@/conf/conf.module';
+
+const entitiesArray = [Admin, 'dist/src/entities/*.entity.{ts,js}'];
+
+const typeOrmFactory = (confService: ConfService): TypeOrmModuleOptions => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { type, ...postgres } = confService.env().postgres;
+  return {
+    type: 'postgres',
+    ...postgres,
+    entities: entitiesArray,
+  };
+};
 
 @Module({
-	imports: [
-		TypeOrmModule.forRootAsync({
-			useFactory: async (confService: ConfService) => {
-				const { type, ...postgres } = confService.env().postgres;
-				return {
-					type: 'postgres',
-					...postgres,
-					entities: [Admin, 'dist/src/entities/*.entity.{ts,js}'],
-				}
-			},
-			inject: [ConfService],
-		}),
-		TypeOrmModule.forFeature([]),
-	],
-	providers: [ConfService],
-	exports: [TypeOrmModule],
+  imports: [
+    ConfModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfModule],
+      useFactory: typeOrmFactory,
+      inject: [ConfService],
+    }),
+    TypeOrmModule.forFeature(),
+  ],
+  exports: [TypeOrmModule],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
